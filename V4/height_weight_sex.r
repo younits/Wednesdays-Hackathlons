ad20c41a22b06981088dcf102222a820a1dbf62b
Younits <laalou.younes@gmail.com>
2024-08-21 10:45:41 +0200

    feat: Add height-weight simulation function

--- V4/height_weight_sex.r
+++ V4/height_weight_sex.r
@@ -1,3 +1,11 @@
+# install.packages(c("coda", "mvtnorm", "devtools", "dagitty"))
+# install.packages("Rlab")
+library(devtools)
+library(Rlab)
+# devtools::install_github("rmcelreath/rethinking")
+
+# S <- sample(c(1,2), 20, replace = TRUE)
+
 # S=1 female; S=2 male
 sim_HW <- function(S, b, a) { # nolint
   N <- length(S)
@@ -6,3 +14,7 @@ sim_HW <- function(S, b, a) { # nolint
   # for each S/Sex, there is a different height-weight line
   data.frame(S, H, W)
 }
+
+S <- rbern(100, 1) + 1       #rbern = 100 individuals; drawn male or female at random 
+dat <- sim_HW(S, b = c(0.5, 0.6), a = c(0,0))   # simulates HW
+head(dat)
